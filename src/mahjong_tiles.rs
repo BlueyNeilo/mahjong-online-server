b8963ac976eccfd4276e1f0d9f384bab6608aba0
BlueyNeilo <bluey.neilo@gmail.com>
2017-12-12 20:38:23 +1100

    A module for the tiles for server and client

---
+++ src/mahjong_tiles.rs
@@ -0,0 +1,32 @@
+//This Module has the data for the tile types
+
+trait Simple;
+
+enum Dragon {
+    Red,
+    Green,
+    White
+}
+enum Wind {
+    North,
+    South,
+    East,
+    West
+}
+
+enum Simple {
+    Dot(u8),
+    Bamboo(u8),
+    Character(u8)
+}
+
+enum Honour {
+    W(Wind),
+    D(Dragon)
+}
+
+enum Tile {
+    S(Simple)
+    H(Honour),
+    Flower(u8)
+}
