d85aa26783fd176abdab1f8c2c01ad1c1bc0b305
BlueyNeilo <bluey.neilo@gmail.com>
2017-12-31 17:55:41 +1100

    Reworked the tile representation

--- src/mahjong_tiles.rs
+++ src/mahjong_tiles.rs
@@ -1,30 +1,86 @@
 //This Module has the data for the tile types
 
-enum Dragon {
+//These enums are treated like C enums
+enum Dragons {
     Red,
     Green,
     White
 }
-enum Wind {
+enum Winds {
     North,
     South,
     East,
     West
 }
 
-enum Simple {
-    Dot(u8),
-    Bamboo(u8),
-    Character(u8)
+enum SimpleSuit {
+    Dot,
+    Bamboo,
+    Character,
 }
 
-enum Honour {
-    W(Wind),
-    D(Dragon)
+enum HonourSuit {
+    Wind,
+    Dragon,
 }
 
-enum Tile {
-    S(Simple),
-    H(Honour),
-    Flower(u8)
+enum HonourValue {
+    Wind(Winds),
+    Dragon(Dragons),
+}
+
+struct FlowerSuit;
+
+trait Tile {
+    type TileSuit;
+    type TileValue;
+    fn new(s: Self::TileSuit, v: Self::TileValue) -> Self;
+}
+
+struct SimpleTile {
+    suit : SimpleSuit,
+    value : u8,
+}
+
+struct HonourTile {
+    suit : HonourSuit,
+    value : HonourValue,
+}
+
+struct FlowerTile {
+    suit : FlowerSuit,
+    value : u8,
+}
+
+impl Tile for SimpleTile {
+    type TileSuit = SimpleSuit;
+    type TileValue = u8;
+    fn new(s: Self::TileSuit, v: Self::TileValue) -> Self {
+        Self {
+            suit: s,
+            value: v,
+        }
+    }
+}
+
+impl Tile for HonourTile {
+    type TileSuit = HonourSuit;
+    type TileValue = HonourValue;
+    fn new(s: Self::TileSuit, v: Self::TileValue) -> Self {
+        Self {
+            suit: s,
+            value: v,
+        }
+    }
+}
+
+impl Tile for FlowerTile {
+    type TileSuit = FlowerSuit;
+    type TileValue = u8;
+    fn new(s: Self::TileSuit, v: Self::TileValue) -> Self {
+        Self {
+            suit: s,
+            value: v,
+        }
+    }
 }
